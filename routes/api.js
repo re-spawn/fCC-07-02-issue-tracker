a8d19abb7feb97ee2e67e2900c833ed29c89ab02
re-spawn <rob@robanthony.ca>
2023-01-19 18:11:20 -0330

    feat: POST, GET and partial PUT

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,169 @@
 'use strict';
 
-module.exports = function (app) {
+const fs = require('fs');
+const db = './db';
 
+module.exports = function (app) {
+  
   app.route('/api/issues/:project')
   
     .get(function (req, res){
       let project = req.params.project;
-      
+      if (fs.existsSync(db)) {
+        let projects = JSON.parse(fs.readFileSync(db));
+        let projectIndex = projects.findIndex((element) => {
+          return element.project == project;
+        });
+        if (projectIndex == -1) {
+          res.json([]);
+          console.log("GET: No issues for project " + project);
+        } else {
+          let issues = [...projects[projectIndex].issues];
+          Object.keys(req.query).forEach((key) => {
+            issues = issues.filter((issue) => {
+              return issue[key] == req.query[key];
+            });
+          });
+          res.json([...issues]);
+          console.log("GET: issues", issues);
+        }
+      } else {
+        res.json([]);
+        console.log("GET: No issues DB");
+      }
     })
     
     .post(function (req, res){
+      // Projects DB
       let project = req.params.project;
-      
+      if (fs.existsSync(db)) {
+        var projects = JSON.parse(fs.readFileSync(db));
+      } else {
+        var projects = [];
+      }
+      let projectIndex = projects.findIndex((element) => {
+        return element.project == project;
+      });
+      if (projectIndex == -1) {
+        projectIndex = projects.length;
+        projects.push({
+          "project": project,
+          "issues": []
+        });
+      }
+      // Required Fields
+      const issueTitle = req.body.issue_title;
+      const issueText = req.body.issue_text;
+      const createdBy = req.body.created_by;
+      if (issueTitle == undefined || issueTitle == '' ||
+          issueText == undefined || issueText == '' ||
+          createdBy == undefined || createdBy == '') {
+        res.sendStatus(400);
+        return;
+      }
+      // Optional Fields
+      const assignedTo = (req.body.assigned_to == undefined) ? "" : req.body.assigned_to;
+      const statusText = (req.body.status_text == undefined) ? "" : req.body.status_text;
+      let date = new Date();
+      // Issue
+      let issue = {
+        "_id": project + (projects[projectIndex].issues.length + 1),
+        "issue_title": issueTitle,
+        "issue_text": issueText,
+        "created_on": date.toUTCString(),
+        "updated_on": date.toUTCString(),
+        "created_by": createdBy,
+        "assigned_to": assignedTo,
+        "open": true,
+        "status_text": statusText,
+      };
+      projects[projectIndex].issues.push({...issue});
+      fs.writeFileSync(db, JSON.stringify(projects));
+      res.json({...issue});
+      console.log("POST: projects[projectIndex].project", projects[projectIndex].project);
+      console.log("POST: projects[projectIndex].issues", projects[projectIndex].issues);
     })
     
     .put(function (req, res){
       let project = req.params.project;
-      
+      if (fs.existsSync(db)) {
+        let projects = JSON.parse(fs.readFileSync(db));
+        let projectIndex = projects.findIndex((element) => {
+          return element.project == project;
+        });
+        if (projectIndex == -1) {
+          res.json({
+            "error": "could not update",
+            "_id": req.body._id
+          });
+          console.log("PUT: No issues for project " + project);
+        } else {
+          let issueIndex = projects[projectIndex].issues.findIndex((issue) => {
+            return issue._id == req.body._id;
+          });
+          if (issueIndex == -1) {
+            res.json({
+              "error": "could not update",
+              "_id": req.body._id
+            });
+          } else {
+            Object.keys(req.body).forEach((key) => {
+              if (req.body[key] == "") {
+                if (key == "issue_title" ||
+                    key == "issue_text" ||
+                    key == "created_by" ||
+                    key == "open") {
+                  res.json({
+                    "error": "could not update",
+                    "_id": req.body._id
+                  });
+                  console.log("PUT: Required field cannot be set to empty string");
+                } else {
+                  projects[projectIndex].issues[issueIndex][key] = req.body[key];
+                }
+              } else if (key == "open" && typeof req.body[key] != "boolean") {
+                res.json({
+                  "error": "could not update",
+                  "_id": req.body._id
+                });
+                console.log("PUT: Open must be boolean");
+              } else {
+                projects[projectIndex].issues[issueIndex][key] = req.body[key];
+              }
+            });
+          }
+          res.json({
+            "result": "successfully updated",
+            "_id": req.body._id
+          });
+          console.log("PUT: updated issue", projects[projectIndex].issues[issueIndex]);
+        }
+      } else {
+        res.json({
+          "error": "could not update",
+          "_id": req.body._id
+        });
+        console.log("PUT: Issues DB missing!");
+      }
     })
     
     .delete(function (req, res){
       let project = req.params.project;
       
     });
+
+    /* TEMPLATES ***********************
+    {
+      '_id': 'string',
+      'issue_title': 'string',
+      'issue_text': 'string',
+      'created_on': 'date',
+      'updated_on': 'date',
+      'created_by': 'string',
+      'assigned_to': 'string',
+      'open': boolean,
+      'status_text': 'string'
+    }
+    ********************************* */
     
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,372 @@
 const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
-const server = require('../server');
+// const server = require('../server');
+
+require('dotenv').config();
+if (process.env.HOST === 'repl') {
+  var server = 'https://fCC-07-02-issue-tracker.respawn709.repl.co';
+} else {
+  var server = require('../server');
+}
 
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  
+
+  this.timeout(5000);
+  const route = '/api/issues/re-spawn';
+  const ids = [];
+
+  test('Create an issue with every field', function(done) {
+    const issueTitle = 'My First Issue Title';
+    const issueText = 'My First Issue Text - blah, blah, blah';
+    const createdBy = 'Rob';
+    const assignedTo = 'Rob';
+    const statusText = 'Pending';
+    chai
+      .request(server)
+      .post(route)
+      .type('form')
+      .send({
+        'issue_title': issueTitle,
+        'issue_text': issueText,
+        'created_by': createdBy,
+        'assigned_to': assignedTo,
+        'status_text': statusText
+      })
+      .end(function(err, res) {
+        if (err) {
+          console.error(err);
+        } else {
+          assert.strictEqual(res.status, 200, 'Response status not 200 (OK)');
+          // _id
+          assert.isString(res.body._id);
+          assert.notInclude(ids, res.body._id);
+          ids.push(res.body._id);
+          // issue_title
+          assert.strictEqual(res.body.issue_title, issueTitle);
+          // issue_text
+          assert.strictEqual(res.body.issue_text, issueText);
+          // created_on
+	  assert.isDefined(res.body.created_on);
+          assert.isNotEmpty(res.body.created_on);
+          const createdOn = new Date(res.body.created_on);
+          assert.instanceOf(createdOn, Date);
+	  assert.isNotNaN(createdOn);
+          assert.isAtMost(createdOn, new Date());
+          // updated_on
+	  assert.isDefined(res.body.updated_on);
+          assert.isNotEmpty(res.body.updated_on);
+          const updatedOn = new Date(res.body.updated_on);
+          assert.instanceOf(updatedOn, Date);
+	  assert.isNotNaN(updatedOn);
+          assert.strictEqual(updatedOn.toUTCString(), createdOn.toUTCString());
+          // created_by
+          assert.strictEqual(res.body.created_by, createdBy);
+          // assigned_to
+          assert.strictEqual(res.body.assigned_to, assignedTo);
+          // open
+          assert.isTrue(res.body.open);
+          // status_text
+          assert.strictEqual(res.body.status_text, statusText);
+        }
+        done();
+      });
+  });
+  test('Create an issue with only required fields', function(done) {
+    const issueTitle = 'My Second Issue Title';
+    const issueText = 'My Second Issue Text - blah, blah, blah';
+    const createdBy = 'Rob';
+    const assignedTo = '';
+    const statusText = '';
+    chai
+      .request(server)
+      .post(route)
+      .type('form')
+      .send({
+        'issue_title': issueTitle,
+        'issue_text': issueText,
+        'created_by': createdBy,
+      })
+      .end(function(err, res) {
+        if (err) {
+          console.error(err);
+        } else {
+          assert.strictEqual(res.status, 200, 'Response status not 200 (OK)');
+          // _id
+          assert.isString(res.body._id);
+          assert.notInclude(ids, res.body._id);
+          ids.push(res.body._id);
+          // issue_title
+          assert.strictEqual(res.body.issue_title, issueTitle);
+          // issue_text
+          assert.strictEqual(res.body.issue_text, issueText);
+          // created_on
+	  assert.isDefined(res.body.created_on);
+          assert.isNotEmpty(res.body.created_on);
+          const createdOn = new Date(res.body.created_on);
+          assert.instanceOf(createdOn, Date);
+	  assert.isNotNaN(createdOn);
+          assert.isAtMost(createdOn, new Date());
+          // updated_on
+	  assert.isDefined(res.body.updated_on);
+          assert.isNotEmpty(res.body.updated_on);
+          const updatedOn = new Date(res.body.updated_on);
+          assert.instanceOf(updatedOn, Date);
+	  assert.isNotNaN(updatedOn);
+          assert.strictEqual(updatedOn.toUTCString(), createdOn.toUTCString());
+          // created_by
+          assert.strictEqual(res.body.created_by, createdBy);
+          // assigned_to
+          assert.strictEqual(res.body.assigned_to, assignedTo);
+          // open
+          assert.isTrue(res.body.open);
+          // status_text
+          assert.strictEqual(res.body.status_text, statusText);
+        }
+        done();
+      });
+  });
+  test('Create another issue with only required fields', function(done) {
+    const issueTitle = 'My First Issue Title';
+    const issueText = 'My First Issue Text - blah, blah, blah';
+    const createdBy = 'George';
+    const assignedTo = '';
+    const statusText = '';
+    chai
+      .request(server)
+      .post(route)
+      .type('form')
+      .send({
+        'issue_title': issueTitle,
+        'issue_text': issueText,
+        'created_by': createdBy,
+      })
+      .end(function(err, res) {
+        if (err) {
+          console.error(err);
+        } else {
+          assert.strictEqual(res.status, 200, 'Response status not 200 (OK)');
+          // _id
+          assert.isString(res.body._id);
+          assert.notInclude(ids, res.body._id);
+          ids.push(res.body._id);
+          // issue_title
+          assert.strictEqual(res.body.issue_title, issueTitle);
+          // issue_text
+          assert.strictEqual(res.body.issue_text, issueText);
+          // created_on
+	  assert.isDefined(res.body.created_on);
+          assert.isNotEmpty(res.body.created_on);
+          const createdOn = new Date(res.body.created_on);
+          assert.instanceOf(createdOn, Date);
+	  assert.isNotNaN(createdOn);
+          assert.isAtMost(createdOn, new Date());
+          // updated_on
+	  assert.isDefined(res.body.updated_on);
+          assert.isNotEmpty(res.body.updated_on);
+          const updatedOn = new Date(res.body.updated_on);
+          assert.instanceOf(updatedOn, Date);
+	  assert.isNotNaN(updatedOn);
+          assert.strictEqual(updatedOn.toUTCString(), createdOn.toUTCString());
+          // created_by
+          assert.strictEqual(res.body.created_by, createdBy);
+          // assigned_to
+          assert.strictEqual(res.body.assigned_to, assignedTo);
+          // open
+          assert.isTrue(res.body.open);
+          // status_text
+          assert.strictEqual(res.body.status_text, statusText);
+        }
+        done();
+      });
+  });
+  test('Create an issue with missing required field issue_title', function(done) {
+    // const issueTitle = 'My Third Issue Title';
+    const issueTitle = '';
+    const issueText = 'My Third Issue Text - blah, blah, blah';
+    const createdBy = 'Rob';
+    const assignedTo = '';
+    const statusText = '';
+    chai
+      .request(server)
+      .post(route)
+      .type('form')
+      .send({
+        'issue_title': issueTitle,
+        'issue_text': issueText,
+        'created_by': createdBy,
+      })
+      .end(function(err, res) {
+        if (err) {
+          console.error(err);
+        } else {
+          assert.strictEqual(res.status, 400, 'Response status not 400 (Bad Request)');
+        }
+        done();
+      });
+  });
+  test('Create an issue with missing required field issue_text', function(done) {
+    const issueTitle = 'My Fouth Issue Title';
+    // const issueText = 'My Fourth Issue Text - blah, blah, blah';
+    const issueText = '';
+    const createdBy = 'Rob';
+    const assignedTo = '';
+    const statusText = '';
+    chai
+      .request(server)
+      .post(route)
+      .type('form')
+      .send({
+        'issue_title': issueTitle,
+        'issue_text': issueText,
+        'created_by': createdBy,
+      })
+      .end(function(err, res) {
+        if (err) {
+          console.error(err);
+        } else {
+          assert.strictEqual(res.status, 400, 'Response status not 400 (Bad Request)');
+        }
+        done();
+      });
+  });
+  test('Create an issue with missing required field created_by', function(done) {
+    const issueTitle = 'My Fifth Issue Title';
+    const issueText = 'My Fifth Issue Text - blah, blah, blah';
+    // const createdBy = 'Rob';
+    const createdBy = '';
+    const assignedTo = '';
+    const statusText = '';
+    chai
+      .request(server)
+      .post(route)
+      .type('form')
+      .send({
+        'issue_title': issueTitle,
+        'issue_text': issueText,
+        'created_by': createdBy,
+      })
+      .end(function(err, res) {
+        if (err) {
+          console.error(err);
+        } else {
+          assert.strictEqual(res.status, 400, 'Response status not 400 (Bad Request)');
+        }
+        done();
+      });
+  });
+  test('Get all issues', function(done) {
+    chai
+      .request(server)
+      .get(route)
+      .end(function(err, res) {
+        if (err) {
+          console.error(err);
+        } else {
+          assert.strictEqual(res.status, 200, 'Response status not 200 (OK)');
+          assert.isArray(res.body);
+          for (let id = 0; id < ids.length; id++) {
+            let issueIndex = res.body.findIndex((element) => {
+              return element._id == ids[id];
+	    });
+            assert.notEqual(issueIndex, -1);
+          }
+        }
+        done();
+      });
+  });
+  test('Get issues using one filter', function(done) {
+    let issueTitle = 'My First Issue Title';
+    chai
+      .request(server)
+      .get(route + '?issue_title=' + issueTitle)
+      .end(function(err, res) {
+        if (err) {
+          console.error(err);
+        } else {
+          assert.strictEqual(res.status, 200, 'Response status not 200 (OK)');
+          let issues = res.body;
+          assert.isArray(issues);
+          for (let i = 0; i < issues.length; i++) {
+            assert.strictEqual(issues[i].issue_title, issueTitle);
+          }
+        }
+        done();
+      });
+  });
+  test('Get issues using multiple filters', function(done) {
+    let issueTitle = 'My First Issue Title';
+    let createdBy = 'George';
+    chai
+      .request(server)
+      .get(route + '?issue_title=' + issueTitle + '&created_by=' + createdBy)
+      .end(function(err, res) {
+        if (err) {
+          console.error(err);
+        } else {
+          assert.strictEqual(res.status, 200, 'Response status not 200 (OK)');
+          let issues = res.body;
+          assert.isArray(issues);
+          for (let i = 0; i < issues.length; i++) {
+            assert.strictEqual(issues[i].issue_title, issueTitle);
+            assert.strictEqual(issues[i].created_by, createdBy);
+          }
+        }
+        done();
+      });
+  });
+  test('Update one field', function(done) {
+    let _id = 're-spawn1';
+    let assignedTo = 'George';
+    chai
+      .request(server)
+      .put(route)
+      .type('form')
+      .send({
+        '_id': _id,
+        'assigned_to': assignedTo
+      })
+      .end(function(err, res) {
+        if (err) {
+          console.error(err);
+        } else {
+          assert.strictEqual(res.status, 200, 'Response status not 200 (OK)');
+          assert.strictEqual(res.body.result, 'successfully updated');
+          assert.strictEqual(res.body._id, _id);
+        }
+        done();
+      });
+  });
+ 
+  /* TEMPLATES *************************
+  {
+    '_id': 'string',
+    'issue_title': 'string',
+    'issue_text': 'string',
+    'created_on': 'date',
+    'updated_on': 'date',
+    'created_by': 'string',
+    'assigned_to': 'string',
+    'open': boolean,
+    'status_text': 'string'
+  }
+  test('TEST_DESCRIPTION', function(done) {
+    chai
+      .request(server)
+      .ROUTE(route)
+      ...
+      .end(function(err, res) {
+        if (err) {
+          console.error(err);
+        } else {
+          assert.strictEqual(res.status, 200, 'Response status not 200 (OK)');
+          assert.ASSERTION(res.body.PROP, EXPECTED);
+        }
+        done();
+      });
+  });
+  *********************************** */
+
 });
